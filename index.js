7df6a7c33caa8c7fc31697cb7171cbf4857d5134
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-05 22:28:01 +0000

    Remove commit. Create value getters in subclasses.

--- index.js
+++ index.js
@@ -5,54 +5,32 @@ class Account {
   }
 }
 
-class Withdrawal {
-
+class Transaction {
   constructor(amount, account) {
-    this.amount = amount;
-    this.account = account;
-  }
-
-  commit() {
-    this.account.balance -= this.amount;
+    this.ammount = amount;        // amount passed in for transaction
+    this.account = account;       // object (instance of Account class) passed in -->  myAccount = { username: 'meow patrol', balance: 0 };
   }
 
 }
 
-class Deposit {
-  constructor(amount, account) {
-    this.amount = amount;
-    this.account = account;
-  }
-  commit() {
-    this.account.balance += this.amount;
+class Withdrawal extends Transaction { // takes 2 values: amount, account
+  get value() {
+    return -this.amount;        //
   }
 }
 
-
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+}
 
 
 
 // DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
 const myAccount = new Account('meow-patrol');
 
-t1 = new Withdrawal(50.25, myAccount);
-t1.commit();
-// console.log('Transaction 1:', t1);
-console.log(1, myAccount.balance);
-
-
-t2 = new Withdrawal(9.99, myAccount);
-t2.commit();
-// console.log('Transaction 2:', t2);
-console.log(2, myAccount.balance);
-
-
-
-t3 = new Deposit(120.00, myAccount);
-t3.commit();
-// console.log('T3:', t3);
-console.log(3, myAccount.balance);
-
+const t1 = new Withdrawal('10', myAccount);
+console.log(t1);
 
 
