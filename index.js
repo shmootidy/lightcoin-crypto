c7544fbb6e5465a3691f2116f3f971586156740f
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-05 22:03:45 +0000

    Use account object in Withdrawl and Deposit classes.

--- index.js
+++ index.js
@@ -7,22 +7,24 @@ class Account {
 
 class Withdrawal {
 
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance -= this.amount;
   }
 
 }
 
 class Deposit {
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
   commit() {
-    balance += this.amount;
+    this.account.balance += this.amount;
   }
 }
 
@@ -32,22 +34,25 @@ class Deposit {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account('meow-patrol');
 
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+// console.log('Transaction 1:', t1);
+console.log(1, myAccount.balance);
 
-t2 = new Withdrawal(9.99);
+
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+// console.log('Transaction 2:', t2);
+console.log(2, myAccount.balance);
 
-console.log('Balance:', balance);
 
 
-t3 = new Deposit(120.00);
+t3 = new Deposit(120.00, myAccount);
 t3.commit();
-console.log('T3:', t3);
-console.log(balance);
+// console.log('T3:', t3);
+console.log(3, myAccount.balance);
 
 
 
